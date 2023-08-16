ec32d88c9ea42bebc8833e99ba6df36d1cd8dc57
Kev <kevinhen10@gmail.com>
2023-08-16 19:56:38 -0300

    Feat: Footer component.

---
+++ src/client/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import Video from "@/client/components/video/video";
+
+export default function Footer() {
+  return (
+        <div className="bg-white h-36 mt-10">
+            Footer
+        </div>
+    )
+}
