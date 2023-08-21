2cbd38d4a188585482021f02dfd7b5d739a269ac
Kev <kevinhen10@gmail.com>
2023-08-21 17:24:03 -0300

    Feat: Product page.

--- src/app/(market)/products/[id]/page.tsx
+++ src/app/(market)/products/[id]/page.tsx
@@ -1,4 +1,8 @@
 'use client';
+import Button from "@/client/components/input/button";
+import ColorSelector from "@/client/components/product/color-selector";
+import ImageSelector from "@/client/components/product/image-selector";
+import Selector from "@/client/components/product/selector";
 import useFetch from "@/client/hooks/useFetch";
 
 type Props = {
@@ -20,8 +24,43 @@ export default function ProductPage({ params }: Props) {
     const { data: product, isFetching } = useFetch<Product>(req);
 
     return (
-        <div className="h-screen 2-screen bg-gray-200">
-            {product && product.title}
+        <div className="h-screen pt-12 bg-gray-200">
+            <div className="h-full w-full flex p-5">
+                <div className="w-full">
+                    <div className="h-3/4 w-full rounded-lg bg-white p-4">
+                        <div className="text-2xl">
+                            Iphone pro max 
+                        </div>
+                        <div className="h-[90%] flex justify-center items-center">
+                            <ImageSelector />
+                        </div>
+                    </div>
+                    <div className="h-2"/>
+                    <div className="h-1/4 flex gap-2">
+                        <div className="w-1/3 h-full bg-white rounded-lg">
+                            <Selector options={['64gb','128gb','256gb','512gb']} type="Armazenamento"/>
+                        </div>
+                        <div className="w-1/3 h-full bg-white rounded-lg">
+                            <ColorSelector options={['blue','red','black','gray']} type="Cores"/>
+                        </div>
+                        <div className="w-1/3 h-full p-3 flex flex-col justify-end items-end
+                        bg-white rounded-lg">
+                            <div/>
+                            <div className=" text-right">
+                                <div className="flex text-gray-500 line-through text-sm">
+                                    <p>R$</p>
+                                    <p>10000</p>
+                                </div>
+                                <div className="flex font-semibold text-lg">
+                                    <p>R$</p>
+                                    <p>100000</p>
+                                </div>
+                            </div>
+                            <Button> Comprar </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/client/components/product/container.tsx
@@ -0,0 +1,8 @@
+export default function Container() {
+
+    return (
+        <div className="h-full p-3 flex flex-col justify-between
+        bg-white rounded-lg">
+        </div>
+    )
+}
