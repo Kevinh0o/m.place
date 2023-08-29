2e79c16bba20eac01018ab92fbb846f0549af61e
Kev <kevinhen10@gmail.com>
2023-08-29 12:04:07 -0300

    Feat: Product component an search pages are now connected with db.

--- src/app/(market)/products/page.tsx
+++ src/app/(market)/products/page.tsx
@@ -1,19 +1,76 @@
+'use client';
 import FilterSelector from "@/client/components/store/filter-selector";
 import Pagination from "@/client/components/store/pagination";
 import Product from "@/client/components/store/product";
+import ProductLoading from "@/client/components/store/product-loading";
+import { FilterContext } from "@/client/contexts/product-filter";
+import useFetch from "@/client/hooks/useFetch";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useContext, useEffect } from "react";
+
+type Product = {
+    id: string;
+    title: string;
+    price: number;
+    discount: number;
+    images: string[];
+}
 
 export default function Products() {
+    const searchParams = useSearchParams();
+    //Get params of the url
+    const search = searchParams?.get('search');
+    const minPrice = searchParams?.get('minPrice');
+    const maxPrice = searchParams?.get('maxPrice');
+    const brand = searchParams?.get('brand');
+    const sortby = searchParams?.get('sortby');
+    const page = searchParams?.get('page');
+
+    const req = {
+        url: '/api/product?' + 
+            'search=' + search +
+            '&minPrice=' + minPrice +
+            '&maxPrice=' + maxPrice + 
+            '&brand=' + brand +
+            '&sortby=' + sortby +
+            '&page=' + page,
+        cache: 'product-' + maxPrice + minPrice + sortby + brand + search + page,
+    };
+
+    const { data: products, isFetching } = useFetch<Product[]>(req);
 
     return (
-        <div className="min-h-screen pt-12 bg-gray-200">
-            <div className="h-full w-full p-5 flex justify-center items-start gap-2">
+        <div className="min-h-screen pt-12 bg-gray-200 flex flex-col justify-between 
+        items-center">
+            <div className="h-full max-w-screen-xl p-5
+            flex justify-between items-start gap-2">
                 <FilterSelector/>
                 <div className="w-full rounded-lg gap-2 flex flex-wrap">
-                    <Product/>
+                    {isFetching ? 
+                        <>
+                            <ProductLoading/>
+                            <ProductLoading/>
+                            <ProductLoading/>
+                        </> :
+                        products?.map((product)=>{
+                            return(
+                                <Product 
+                                    key={product.id}
+                                    title={product.title}
+                                    id={product.id}
+                                    price={product.price}
+                                    discount={product.discount}
+                                    images={product.images}
+                                />
+                            )
+                        })
+                    }               
                 </div>
             </div>
-            <div className="h-full flex items-center justify-center">
-                <Pagination/>
+            <div className="w-full flex items-center justify-center">
+                {products && 
+                <Pagination numberOfProducts={products.length}/>
+                }
             </div>
         </div>
     )
--- src/client/components/store/product.tsx
+++ src/client/components/store/product.tsx
@@ -2,34 +2,39 @@ import Image from "next/image"
 import Link from "next/link"
 
 type Props = {
+    title: string;
+    id: string;
+    price: number;
+    discount: number;
+    images: string[];
 }
 
-export default function Product() {
+export default function Product({ title, id, price, discount, images }: Props) {
 
     return (
         <Link
-            href="/products"
+            href={'/products/' + id}
             className="w-[230px] h-[265px] bg-white rounded-md flex flex-col justify-between
             p-1"
         >
             <Image
-                src="/product-placeholder.png"
-                alt="Picture of the author"
+                src={images[0]}
+                alt={'Imagem do produto ' + title}
                 width={230} 
-                height={200} 
+                height={200}
                 className="w-full h-[150px] rounded-md object-scale-down"
             />
             <h1 className="text-sm font-semibold text-gray-700 px-2">
-                Iphone 14 Pro Max Iphone 14 Pro Max Iph 180gb
+                {title}
             </h1>
             <div>
                 <div className="text-xs text-gray-600 px-2 flex line-through">
                     <p> R$ </p>
-                    <p> 1.000,00 </p>
+                    <p> {discount} </p>
                 </div>
                 <div className="text-md font-bold text-gray-700 px-2 flex">
                     <p> R$ </p>
-                    <p> 1.000,00 </p>
+                    <p> {price} </p>
                 </div>
             </div>
         </Link>
