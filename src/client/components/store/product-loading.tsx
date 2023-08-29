043a54f4542a253ca36aa9073286be0eb0668690
Kev <kevinhen10@gmail.com>
2023-08-29 12:03:25 -0300

    Feat: Product loading component.

---
+++ src/client/components/store/product-loading.tsx
@@ -0,0 +1,9 @@
+
+export default function ProductLoading() {
+
+    return (
+        <div className="w-[230px] h-[265px] bg-white rounded-md flex flex-col 
+        justify-betweenp-1">
+        </div>
+    )
+}
