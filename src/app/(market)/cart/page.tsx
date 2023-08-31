e0a199b4607fa5c6da240a7e4a77ff81b5c2b038
Kev <kevinhen10@gmail.com>
2023-08-31 09:55:52 -0300

    Feat: Cart page an items component + using localstorage.

---
+++ src/app/(market)/cart/page.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import CartItem from "@/client/components/cart/cart-item";
+import Button from "@/client/components/input/button";
+import useLocalStorage from "@/client/hooks/useLocalStorage";
+
+type Props = {
+}
+
+export default function Cart() {
+
+    //TODO prevent duplicates, sum of all items
+    const items = useLocalStorage({ key: 'cart', item: '', method: 'get' });
+
+    return (
+        <div className="p-5 pt-16 bg-gray-200 min-h-screen flex justify-center
+        gap-2">
+            <div className="w-1/2 min-h-full bg-white p-2 rounded-lg flex flex-col gap-2">
+                {items && items.map((i: string)=>{
+                    return(
+                        <CartItem id={i} key={i}/>
+                    )
+                })}
+            </div>
+            <div className="w-[350px] bg-white rounded-lg p-4 self-start">
+                <div className="flex justify-between items-end">
+                    <p>Desconto:</p>
+                    <div className="flex text-emerald-500">
+                        <p className="text-lg">R$ -</p>
+                        <p className="text-lg"> 100,000</p>
+                    </div>
+                </div>
+                <div className="flex justify-between items-end">
+                    <p>subTotal:</p>
+                    <div className="flex">
+                        <p className="text-lg">R$</p>
+                        <p className="text-lg"> 100,000</p>
+                    </div>
+                </div>
+                <div className="flex justify-between items-end">
+                    <p>Total:</p>
+                    <div className="flex">
+                        <p className="text-lg">R$</p>
+                        <p className="text-lg"> 100,000</p>
+                    </div>
+                </div>
+                <Button type="button"> Finalizar compra </Button>
+            </div>
+        </div>
+    )
+}
---
+++ src/client/components/cart/cart-item.tsx
@@ -0,0 +1,63 @@
+'use client';
+import useFetch from "@/client/hooks/useFetch";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+    id: string;
+}
+
+type Product = {
+    title: string;
+    discount: number;
+    price: number;
+    images: string[];
+    id: string;
+}
+
+export default function CartItem({ id }: Props) {
+
+    const req = {
+        url: '/api/product/' + id,
+        cache: 'product-' + id
+    }
+    const {data: product, isFetching } = useFetch<Product>(req);
+
+    console.log(product)
+
+    return (
+        <div className="w-full h-[100px] border p-2 flex justify-between items-center gap-2">
+            <div className="border w-[100px] h-full">
+                {product &&
+                    <Image
+                        src={product.images[0]}
+                        alt={"Imagem do produto" + product.title}
+                        width={100}
+                        height={50}
+                        className="object-contain h-full"
+                    />
+                }
+            </div>
+            <div className="h-full">
+                {product &&
+                    <Link 
+                        href={'/products/' + product.id}
+                        className="mouse-pointer hover:underline"
+                    >
+                        <h1> {product.title} </h1>
+                    </Link>
+                }
+            </div>
+            <div className="w-1/4 h-full flex flex-col items-end justify-end">
+                <div className="flex line-through text-gray-500 text-sm">
+                    <p>R$</p>
+                    <p> {product && product.discount} </p>
+                </div>
+                <div className="flex">
+                    <p>R$</p>
+                    <p> {product && product.price} </p>
+                </div>
+            </div>
+        </div>
+    )
+}
