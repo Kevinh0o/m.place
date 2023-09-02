bdae1baba2acde6d6faf936052b9b0a6711cc4c4
Kev <kevinhen10@gmail.com>
2023-09-02 07:47:10 -0300

    Feat: Sum of price/discount of all cart items.

--- src/app/(market)/cart/page.tsx
+++ src/app/(market)/cart/page.tsx
@@ -17,39 +17,67 @@ type Product = {
 }
 
 export default function Cart() {
-    //TODO prevent duplicates, sum of all items
-    const { data: items }  =  useLocalStorage('cart');
+    const [price, setPrice] = useState(0);
+    const [discount, setDiscount] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    const { data: items, push }  =  useLocalStorage('cart');
+
+    useEffect(()=>{
+        setTotal(price - discount);
+    }, [price, discount])
+
+    function handleCLick(){
+        push('1');
+    }
 
     return (
         <div className="p-5 pt-16 bg-gray-200 min-h-screen flex justify-center
         gap-2">
             <div className="w-1/2 min-h-full bg-white p-2 rounded-lg flex flex-col gap-2">
+
+            <button 
+                className="bg-green-600 text-white rounded-md"
+                onClick={handleCLick}
+            >
+                click
+            </button>
+
                 {items && items.map((i: string)=>{
                     return(
-                        <CartItem id={i} key={i}/>
+                        <CartItem 
+                            id={i} 
+                            key={i}
+                            setPriceCounter={setPrice}
+                            priceCounter={price}
+                            setDiscountCounter={setDiscount}
+                            discountCounter={discount}
+                        />
                     )
                 })}
             </div>
             <div className="w-[350px] bg-white rounded-lg p-4 self-start">
                 <div className="flex justify-between items-end">
-                    <p> Desconto: </p>
-                    <div className="flex text-emerald-500">
-                        <p className="text-lg">R$ -</p>
-                        <p className="text-lg"> 100,000</p>
-                    </div>
-                </div>
-                <div className="flex justify-between items-end">
                     <p>subTotal:</p>
                     <div className="flex">
                         <p className="text-lg">R$</p>
-                        <p className="text-lg"> 100,000</p>
+                        <p className="text-lg"> { price }</p>
                     </div>
                 </div>
+                {discount > 0 &&
+                    <div className="flex justify-between items-end">
+                        <p> Desconto: </p>
+                        <div className="flex text-emerald-500">
+                            <p className="text-lg">R$ -</p>
+                            <p className="text-lg"> { discount } </p>
+                        </div>
+                    </div>
+                }
                 <div className="flex justify-between items-end">
                     <p>Total:</p>
                     <div className="flex">
-                        <p className="text-lg">R$</p>
-                        <p className="text-lg"> 100,000</p>
+                        <p className="text-lg"> R$ </p>
+                        <p className="text-lg"> { total } </p>
                     </div>
                 </div>
                 <Button 
--- src/client/components/cart/cart-item.tsx
+++ src/client/components/cart/cart-item.tsx
@@ -3,9 +3,14 @@ import useFetch from "@/client/hooks/useFetch";
 import useLocalStorage from "@/client/hooks/useLocalStorage";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
 
 type Props = {
     id: string;
+    priceCounter: number;
+    setPriceCounter: Function;
+    discountCounter: number;
+    setDiscountCounter: Function;
 }
 
 type Product = {
@@ -16,13 +21,19 @@ type Product = {
     id: string;
 }
 
-export default function CartItem({ id }: Props) {
+export default function CartItem({ 
+    id,
+    priceCounter,
+    setPriceCounter,
+    discountCounter,
+    setDiscountCounter
+}: Props) {
 
     const req = {
         url: '/api/product/' + id,
         cache: 'product-' + id
     }
-    
+
     const {data: product, isFetching} = useFetch<Product>(req);
 
     const { remove } = useLocalStorage('cart');
@@ -30,6 +41,11 @@ export default function CartItem({ id }: Props) {
     function handleCLick(){
         remove(id);
     }
+
+    useEffect(()=>{
+        setPriceCounter((prev: number)=> prev + product?.price || 0);
+        setDiscountCounter((prev: number)=> prev + product?.discount || 0);
+    }, [ , product])
 
     return (
         <div className="w-full h-[100px] border p-2 flex justify-between items-center gap-2">
@@ -45,13 +61,14 @@ export default function CartItem({ id }: Props) {
                 }
             </div>
             <div className="h-full">
-                {product &&
+                {product ?
                     <Link 
                         href={'/products/' + product.id}
                         className="mouse-pointer hover:underline"
                     >
                         <h1> {product.title} </h1>
                     </Link>
+                    : <h1 className="text-sm">  </h1>
                 }
             </div>
             <div className="w-1/4 h-full flex flex-col items-end justify-end">
