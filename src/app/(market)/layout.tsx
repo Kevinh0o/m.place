f8eedcc4f394a17849e3d2bd2b90258e5fb0c717
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-07 03:47:33 -0300

    Refactor: React query provider path changes.

--- src/app/(market)/layout.tsx
+++ src/app/(market)/layout.tsx
@@ -4,8 +4,6 @@ import Header from "@/client/components/header/header"
 import SearchBar from "@/client/components/search-bar/search-bar"
 import HeaderContextProvider from "@/client/contexts/header-context"
 import FilterContextProvider from "@/client/contexts/product-filter";
-import { queryClient } from "@/client/utils/query-client"
-import { QueryClientProvider } from "react-query"
 
 type Props = {
   children: React.ReactNode
@@ -14,7 +12,6 @@ type Props = {
 export default function marketLayout({ children }: Props) {
   return (
     <div className="bg-gray-200">
-      <QueryClientProvider client={queryClient}>
         <HeaderContextProvider>
           <SearchBar/>
           <Header/>
@@ -23,7 +20,6 @@ export default function marketLayout({ children }: Props) {
           {children}
         </FilterContextProvider>
         <Footer/>
-      </QueryClientProvider>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+'use client'
+import AuthContextProvider from '@/client/contexts/auth-context'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '@/client/utils/query-client'
 
 type Props = {
   children: React.ReactNode
@@ -17,7 +21,11 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <QueryClientProvider client={queryClient}>
+          <AuthContextProvider>
+          {children}
+          </AuthContextProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
