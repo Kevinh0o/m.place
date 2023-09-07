473a9239bf61c97a4b45cb589c66eef4d222f7eb
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-07 03:45:34 -0300

    Refactor: Error handling in useFetch hook.

--- src/client/hooks/useFetch.tsx
+++ src/client/hooks/useFetch.tsx
@@ -8,11 +8,11 @@ type Req = {
 
 export default function useFetch<T = unknown>({ url, cache }: Req){
 
-    const { data, isFetching } = useQuery<T>(cache, async() =>{
+    const { data, isFetching, error } = useQuery<T>(cache, async() =>{
         const response = await axios.get(url);
 
         return response.data
     })
 
-    return {data, isFetching}
+    return {data, isFetching, error}
 }
