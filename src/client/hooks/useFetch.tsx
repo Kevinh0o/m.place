b0573c290ca100423e93a6035154e757fd03e5ab
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-15 02:28:32 -0300

    Feat: Adding bearer token for useAuth.

--- src/client/hooks/useFetch.tsx
+++ src/client/hooks/useFetch.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import useLocalStorage from "./useLocalStorage";
 
 type Req = {
     cache: string;
@@ -8,8 +9,18 @@ type Req = {
 
 export default function useFetch<T = unknown>({ url, cache }: Req){
 
+    //Bearer token configuration
+    const { data: bearerToken } = useLocalStorage('token');
+    
+    const config = {
+        headers: {
+            Authorization: `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json'
+        }
+    }
+
     const { data, isFetching, error } = useQuery<T>(cache, async() =>{
-        const response = await axios.get(url);
+        const response = await axios.get(url, config);
 
         return response.data
     })
