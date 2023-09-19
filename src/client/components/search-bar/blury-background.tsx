277ca51da81a5bbd9b781fbd775bbcb8530d0d27
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-19 07:00:20 -0300

    Refactor: Indentation fix.

--- src/client/components/search-bar/blury-background.tsx
+++ src/client/components/search-bar/blury-background.tsx
@@ -8,8 +8,8 @@ export default function BluryBackground({ setVisibility }: Props) {
 
     return (
         <div
-        onClick={()=>setVisibility(false)}
-        className="w-screen h-screen fixed backdrop-blur-lg z-20">
+            onClick={()=>setVisibility(false)}
+            className="w-screen h-screen fixed backdrop-blur-lg z-20">
         </div>
     )
 }
