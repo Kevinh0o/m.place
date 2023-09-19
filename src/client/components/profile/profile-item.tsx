ffb69aa9a12cb559b8f1210e7f578657722cd2a8
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-19 07:00:47 -0300

    Feat: Profile item for /profile

---
+++ src/client/components/profile/profile-item.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+type Props = {
+    title: string;
+    item?: string;
+}
+
+export default function ProfileItem({title, item}: Props) {
+
+    return (
+        <div className="max-w-[650px] p-1 border border-gray-300 rounded-lg flex">
+            <p className="border-r border-gray-300 w-1/2 text-center">
+                {title}
+            </p>
+            <p className="px-2 w-1/2 text-sm">
+                {item}
+            </p>
+        </div>
+    )
+}
