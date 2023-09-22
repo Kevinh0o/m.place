4dbcb7cdffffe9ffda9207e4e811b34cab1a1056
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-22 10:43:04 -0300

    Feat: Comments page in profile

--- src/app/(market)/profile/comments/page.tsx
+++ src/app/(market)/profile/comments/page.tsx
@@ -1,10 +1,42 @@
-type User = {
-}
+'use client';
+import Comment from "@/client/components/profile/comment";
+import { ProfileContext } from "@/client/contexts/profile-context";
+import { useContext } from "react";
 
-export default function Profile() {
+export default function ProfileComments() {
+    const {user} = useContext(ProfileContext);
+    const currentDate = new Date();
+    const comments = [1, 2, 3];
+
+    /**
+     * <Comment
+                                userId={comment.userId} 
+                                date={comment.createdAt}
+                                content={comment.content}
+                            />
+     */
 
     return(
-        <div>
+        <div className="p-2">
+            <div>
+                <h1 className="text-2xl">
+                    Comentários
+                </h1>
+            </div>
+            {comments &&
+                comments.map((comment, index) => {
+                    return(
+                        <div key={index}>
+                            <Comment
+                                productId="22"
+                                userId={'false id'} 
+                                date={currentDate}
+                                content={'bala o produto é bom'}
+                            />
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
