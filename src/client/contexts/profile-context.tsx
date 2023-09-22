d60ac6e4747d19df59007c740358a759bce840c3
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-22 10:45:29 -0300

    Feat: New types and values ProfileContext.

--- src/client/contexts/profile-context.tsx
+++ src/client/contexts/profile-context.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { User } from "@prisma/client";
 import { createContext, useState } from "react"
 
 type Props = {
@@ -8,22 +7,47 @@ type Props = {
 
 type ContextProps = {
     user: User | undefined;
+    setUser: any;
     page: string;
+    pageNumber: number;
+    setPageNumber: any;
+}
+
+type User = {
+    id: string;
+    username: string;
+    type: string;
+    comments: [Comment];
+    orders: [{}];
+}
+
+type Comment = {
+    userId: string;
+    content: string;
+    createdAt: Date;
+    productId: string;
 }
 
 export const ProfileContext = createContext<ContextProps>({
     user: undefined,
-    page: 'profile'
+    setUser: () => {},
+    page: 'profile',
+    pageNumber: 1,
+    setPageNumber: () => {}
 });
 
 export default function ProfileContextProvider({ children }: Props){
     const [user, setUser] = useState<User | undefined>();
     const [page, setPage] = useState<string>('profile');
+    const [pageNumber, setPageNumber] = useState<number>(1);
 
     return(
         <ProfileContext.Provider value={{
             user,
-            page
+            setUser,
+            page,
+            pageNumber,
+            setPageNumber
         }}>
             {children}
         </ProfileContext.Provider>
