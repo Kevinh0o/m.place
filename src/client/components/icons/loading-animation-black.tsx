ead81f40b23b6a83757e959b99e609dc66cf5ba7
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-22 10:40:48 -0300

    Feat: Black loading animation component.

---
+++ src/client/components/icons/loading-animation-black.tsx
@@ -0,0 +1,10 @@
+import Icon from "./icon"
+
+export default function LoadingBlack() {
+
+    return (
+        <>
+            <Icon src="/loading-black.svg" alt="Icone de carregamento" size="sm"/>
+        </>
+    )
+}
