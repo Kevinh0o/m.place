00a989b12d6bd53fae27b2951d21a5048cce001e
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-09-22 10:45:54 -0300

    Feat: usePut hook.

---
+++ src/client/hooks/usePut.tsx
@@ -0,0 +1,50 @@
+import axios, { AxiosResponse } from "axios";
+import useLocalStorage from "./useLocalStorage";
+import { useState } from "react";
+
+type Req = {
+    body: any;
+    url: string;
+}
+
+export default function usePut({ url, body }: Req){
+    const [response, setResponse] = useState<AxiosResponse<any, any>>();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<any>();
+    
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { data: bearerToken } = useLocalStorage('token');
+    
+    const config = {
+        headers: {
+            Authorization: `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const post = async()=>{
+        setLoading(true);
+        clear();
+
+        try{
+            const response = await axios.put(url, JSON.stringify(body), config);
+            setResponse(response);
+            
+            return response;
+        }
+        catch(error: any){
+            setError(error);
+            return error;
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
+    const clear = () => {
+        setResponse(undefined);
+        setError(undefined);
+    }
+
+    return { post, response, error, loading, clear }
+}
