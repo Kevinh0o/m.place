dd6dd2f2fac250a82007d93623d7e8ae0be124fb
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-10-03 15:33:58 -0300

    Refactor: Placeholder values in comments page changed to its actual data.

--- src/app/(market)/profile/comments/page.tsx
+++ src/app/(market)/profile/comments/page.tsx
@@ -4,18 +4,8 @@ import { ProfileContext } from "@/client/contexts/profile-context";
 import { useContext } from "react";
 
 export default function ProfileComments() {
-    const {user} = useContext(ProfileContext);
-    const currentDate = new Date();
-    const comments = [1, 2, 3];
-
-    /**
-     * <Comment
-                                userId={comment.userId} 
-                                date={comment.createdAt}
-                                content={comment.content}
-                            />
-     */
-
+    const { user } = useContext(ProfileContext);
+    
     return(
         <div className="p-2">
             <div>
@@ -23,19 +13,26 @@ export default function ProfileComments() {
                     Comentários
                 </h1>
             </div>
-            {comments &&
-                comments.map((comment, index) => {
+            {user &&
+                user.comments?.map((comment, index) => {
                     return(
                         <div key={index}>
                             <Comment
-                                productId="22"
-                                userId={'false id'} 
-                                date={currentDate}
-                                content={'bala o produto é bom'}
+                                productId={comment.productId}
+                                userId={comment.userId} 
+                                date={comment.createdAt}
+                                content={comment.content}
                             />
                         </div>
                     )
                 })
+            }
+            {!user?.comments &&
+                <div className="p-5">
+                    <p>
+                        Você não tem comentários.
+                    </p>
+                </div>
             }
         </div>
     )
