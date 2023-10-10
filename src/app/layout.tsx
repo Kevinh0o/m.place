1297b170574cbe2b212ecec7d5c9d4518561f2a5
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-10-10 10:12:53 -0300

    Fix: Fixed compile message error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 import AuthContextProvider from '@/client/contexts/auth-context'
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { QueryClientProvider } from 'react-query'
 import { queryClient } from '@/client/utils/query-client'
@@ -11,11 +10,6 @@ type Props = {
 }
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'm.Place',
-  description: 'description',
-}
 
 export default function RootLayout({ children }: Props) {
   return (
